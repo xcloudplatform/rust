8a1b8ef7ad339adf1c33a8a909e14fce146df6ba
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-04-02 18:28:33 +0200

    [SOL] Update build script to automatically build for correct targets

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     exit;
 fi
 
+if [ "$(uname)" == "Darwin" ]; then
+    HOST_TRIPLE=x86_64-apple-darwin
+else
+    HOST_TRIPLE=x86_64-unknown-linux-gnu
+fi
+
 if [ "$1" == "--llvm" ]; then
     rm -f build/x86_64-apple-darwin/llvm/llvm-finished-building;
 fi
-./x.py build --stage 1
+./x.py build --stage 1 --target ${HOST_TRIPLE},bpfel-unknown-unknown
