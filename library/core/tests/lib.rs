9020724d6932b9a48d0ab138cad978d41faecd19
Alessandro Decina <alessandro.d@gmail.com>
2021-11-26 12:23:58 +1100

    [SOL] tests: disable failing core tests

--- library/core/tests/lib.rs
+++ library/core/tests/lib.rs
@@ -111,6 +111,7 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 #![deny(fuzzy_provenance_casts)]
 
+#[cfg(not(target_arch = "bpf"))]
 extern crate test;
 
 mod alloc;
@@ -118,6 +119,7 @@ mod any;
 mod array;
 mod ascii;
 mod asserting;
+#[cfg(not(target_arch = "bpf"))]
 mod atomic;
 mod bool;
 mod cell;
@@ -126,16 +128,20 @@ mod clone;
 mod cmp;
 mod const_ptr;
 mod convert;
+#[cfg(not(target_arch = "bpf"))]
 mod fmt;
 mod future;
+#[cfg(not(target_arch = "bpf"))]
 mod hash;
 mod intrinsics;
 mod iter;
+#[cfg(not(target_arch = "bpf"))]
 mod lazy;
 mod macros;
 mod manually_drop;
 mod mem;
 mod nonzero;
+#[cfg(not(target_arch = "bpf"))]
 mod num;
 mod ops;
 mod option;
@@ -143,9 +149,11 @@ mod panic;
 mod pattern;
 mod pin;
 mod pin_macro;
+#[cfg(not(target_arch = "bpf"))]
 mod ptr;
 mod result;
 mod simd;
+#[cfg(not(target_arch = "bpf"))]
 mod slice;
 mod str;
 mod str_lossy;
